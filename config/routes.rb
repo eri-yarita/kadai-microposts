d8eb4a3f73ce9ef4fa2cc23c20dfc3d47a042d74
eri yarita <jyf4_wxcd63@yahoo.co.jp>
2020-10-19 12:03:52 +0000

    favorite

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,17 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create] do
     member do
       get :followings
       get :followers
+      get :likes
     end
   end
   
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
